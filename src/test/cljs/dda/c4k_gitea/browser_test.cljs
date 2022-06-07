303ad0908810d6877d24fbe617260bd1dc35186e
ansgarz <ansgar.zwick@meissa-gmbh.de>
2022-06-07 18:43:53 +0200

    fix cljs tests

--- src/test/cljs/dda/c4k_gitea/browser_test.cljs
+++ src/test/cljs/dda/c4k_gitea/browser_test.cljs
@@ -1,8 +1,8 @@
-(ns dda.c4k-shynet.browser-test
+(ns dda.c4k-gitea.browser-test
   (:require
    [cljs.test :refer-macros [deftest is are testing run-tests]]
    [hickory.render :as hr]
-   [dda.c4k-shynet.browser :as cut]))
+   [dda.c4k-gitea.browser :as cut]))
 
 (deftest should-generate-group
   (is (= "<div class=\"rounded border border-3  m-3 p-2\"><b style=\"z-index: 1; position: relative; top: -1.3rem;\">id1</b><fieldset>content</fieldset></div>"
