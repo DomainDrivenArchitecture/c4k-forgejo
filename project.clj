474bf9babaea0850e3633e2309acc5361269f7c0
Clemens Geibel <clemens.geibel@meissa-gmbh.de>
2022-08-05 16:24:27 +0200

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.domaindrivenarchitecture/c4k-gitea "0.0.1-SNAPSHOT"
+(defproject org.domaindrivenarchitecture/c4k-gitea "0.1.0"
   :description "gitea c4k-installation package"
   :url "https://domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
