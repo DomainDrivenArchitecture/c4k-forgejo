5002ac874d439d029d4f98bfaaa47282b826186c
Mirco <mirco.zachmann@meissa.de>
2023-03-28 09:46:15 +0200

    Change name from gitea to forgejo

--- doc/Upgrading.md
+++ doc/Upgrading.md
@@ -2,20 +2,20 @@
 
 ## adhoc (on kubernetes cluster)
 
-Ssh into your kubernetes cluster running the gitea instance.  
+Ssh into your kubernetes cluster running the forgejo instance.  
 
 ``` bash
-kubectl edit configmap gitea-env
+kubectl edit configmap forgejo-env
 # make sure INSTALL_LOCK under security is set to true to disable the installation screen
 # save and exit
-kubectl edit deployments gitea
-# search for your current gitea version, e.g. 1.17.0
+kubectl edit deployments forgejo
+# search for your current forgejo version, e.g. 1.17.0
 # replace with new version
 # save and exit
-kubectl scale deployment gitea --replicas=0
-kubectl scale deployment gitea --replicas=1
+kubectl scale deployment forgejo --replicas=0
+kubectl scale deployment forgejo --replicas=1
 ```
 
 Logging into the admin account should now show the new version.
 
-You may want to update your c4k-gitea resources to reflect the changes made on the cluster.
+You may want to update your c4k-forgejo resources to reflect the changes made on the cluster.
--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject org.domaindrivenarchitecture/c4k-gitea "1.0.1-SNAPSHOT"
-  :description "gitea c4k-installation package"
+(defproject org.domaindrivenarchitecture/c4k-forgejo "1.0.1-SNAPSHOT"
+  :description "forgejo c4k-installation package"
   :url "https://domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
@@ -20,8 +20,8 @@
                     :dependencies [[dda/data-test "0.1.1"]]}
              :dev {:plugins [[lein-shell "0.5.0"]]}
              :uberjar {:aot :all
-                       :main dda.c4k-gitea.uberjar
-                       :uberjar-name "c4k-gitea-standalone.jar"
+                       :main dda.c4k-forgejo.uberjar
+                       :uberjar-name "c4k-forgejo-standalone.jar"
                        :dependencies [[org.clojure/tools.cli "1.0.206"]
                                       [ch.qos.logback/logback-classic "1.3.0-alpha4"
                                        :exclusions [com.sun.mail/javax.mail]]
@@ -36,11 +36,11 @@
                       "native-image"
                       "--report-unsupported-elements-at-runtime"
                       "--initialize-at-build-time"
-                      "-jar" "target/uberjar/c4k-gitea-standalone.jar"
+                      "-jar" "target/uberjar/c4k-forgejo-standalone.jar"
                       "-H:ResourceConfigurationFiles=graalvm-resource-config.json"
                       "-H:Log=registerResource"
                       "-H:Name=target/graalvm/${:name}"]
             "inst" ["shell"
                     "sh"
                     "-c"
-                    "lein uberjar && sudo install -m=755 target/uberjar/c4k-gitea-standalone.jar /usr/local/bin/c4k-gitea-standalone.jar"]})
+                    "lein uberjar && sudo install -m=755 target/uberjar/c4k-forgejo-standalone.jar /usr/local/bin/c4k-forgejo-standalone.jar"]})
