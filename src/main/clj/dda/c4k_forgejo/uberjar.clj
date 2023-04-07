5a1d84b441831ed606adfddced4fa7e49c91b77b
bom <mattis.boeckle@meissa-gmbh.de>
2023-04-07 12:03:52 +0200

    Indent main function for readability

--- src/main/clj/dda/c4k_forgejo/uberjar.clj
+++ src/main/clj/dda/c4k_forgejo/uberjar.clj
@@ -2,8 +2,13 @@
   (:gen-class)
   (:require
    [dda.c4k-forgejo.core :as core]
-   [dda.c4k-forgejo.forgejo :as forgejo]
    [dda.c4k-common.uberjar :as uberjar]))
 
 (defn -main [& cmd-args]
-  (uberjar/main-common "c4k-forgejo" core/config? core/auth? core/config-defaults core/k8s-objects cmd-args))
+  (uberjar/main-common 
+   "c4k-forgejo"
+   core/config?
+   core/auth?
+   core/config-defaults
+   core/k8s-objects
+   cmd-args))
