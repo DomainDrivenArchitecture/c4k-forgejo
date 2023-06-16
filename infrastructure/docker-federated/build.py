7e4dbe900b990e744b0b9594e38cbb5fd2fb89ee
bom <mattis.boeckle@meissa-gmbh.de>
2023-06-16 14:20:02 +0200

    Update docker build for federated image

--- infrastructure/docker-federated/build.py
+++ infrastructure/docker-federated/build.py
@@ -1,31 +1,25 @@
 from os import environ
 from pybuilder.core import task, init
 from ddadevops import *
-import logging
 
 name = 'c4k-forgejo-fed'
 MODULE = 'docker'
 PROJECT_ROOT_PATH = '../..'
 
-class MyBuild(DevopsDockerBuild):
-    pass
-
 @init
 def initialize(project):
-    project.build_depends_on('ddadevops>=0.15.5')
-    stage = 'prod'
-    dockerhub_user = environ.get('DOCKERHUB_USER')
-    if not dockerhub_user:
-        dockerhub_user = gopass_field_from_path('meissa/web/docker.com', 'login')
-    dockerhub_password = environ.get('DOCKERHUB_PASSWORD')
-    if not dockerhub_password:
-        dockerhub_password = gopass_password_from_path('meissa/web/docker.com')
-    tag = environ.get('CI_COMMIT_TAG')
-    if not tag:
-        tag = get_tag_from_latest_commit()
-    config = create_devops_docker_build_config(
-        stage, PROJECT_ROOT_PATH, MODULE, dockerhub_user, dockerhub_password, docker_publish_tag=tag)
-    build = MyBuild(project, config)
+    input = {
+        "name": name,
+        "module": MODULE,
+        "stage": "notused",
+        "project_root_path": PROJECT_ROOT_PATH,
+        "build_types": ["IMAGE"],
+        "mixin_types": [],
+    }
+
+    project.build_depends_on('ddadevops>=4.0.0-dev')
+    
+    build = DevopsImageBuild(project, input)
     build.initialize_build_dir()
 
 
