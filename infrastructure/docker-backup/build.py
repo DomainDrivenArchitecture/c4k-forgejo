66dce3dd631c99a91c2363561c81063a67d62cfe
Michael Jerger <michael.jerger@meissa-gmbh.de>
2023-06-16 11:58:40 +0200

    fix the backup image build

--- infrastructure/docker-backup/build.py
+++ infrastructure/docker-backup/build.py
@@ -1,31 +1,26 @@
 from os import environ
 from pybuilder.core import task, init
 from ddadevops import *
-import logging
 
-name = 'c4k-forgejo-backup'
-MODULE = 'docker'
-PROJECT_ROOT_PATH = '../..'
+name = "c4k-forgejo-backup"
+MODULE = "docker"
+PROJECT_ROOT_PATH = "../.."
 
-class MyBuild(DevopsDockerBuild):
-    pass
 
 @init
 def initialize(project):
-    project.build_depends_on('ddadevops>=0.12.4')
-    stage = 'prod'
-    dockerhub_user = environ.get('DOCKERHUB_USER')
-    if not dockerhub_user:
-        dockerhub_user = gopass_field_from_path('meissa/web/docker.com', 'login')
-    dockerhub_password = environ.get('DOCKERHUB_PASSWORD')
-    if not dockerhub_password:
-        dockerhub_password = gopass_password_from_path('meissa/web/docker.com')
-    tag = environ.get('CI_COMMIT_TAG')
-    if not tag:
-        tag = get_tag_from_latest_commit()
-    config = create_devops_docker_build_config(
-        stage, PROJECT_ROOT_PATH, MODULE, dockerhub_user, dockerhub_password, docker_publish_tag=tag)
-    build = MyBuild(project, config)
+    input = {
+        "name": name,
+        "module": MODULE,
+        "stage": "notused",
+        "project_root_path": PROJECT_ROOT_PATH,
+        "build_types": ["IMAGE"],
+        "mixin_types": [],
+    }
+
+    project.build_depends_on("ddadevops>=4.0.0-dev")
+
+    build = DevopsImageBuild(project, input)
     build.initialize_build_dir()
 
 
@@ -34,16 +29,19 @@ def image(project):
     build = get_devops_build(project)
     build.image()
 
+
 @task
 def drun(project):
     build = get_devops_build(project)
     build.drun()
+
 
 @task
 def publish(project):
     build = get_devops_build(project)
     build.dockerhub_login()
     build.dockerhub_publish()
+
 
 @task
 def test(project):
