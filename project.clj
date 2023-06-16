368d16678e86d1bc8458526c0af32b083ca50770
bom <mattis.boeckle@meissa-gmbh.de>
2023-06-16 15:00:54 +0200

    bump version to: 3.0.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.domaindrivenarchitecture/c4k-forgejo "3.0.2"
+(defproject org.domaindrivenarchitecture/c4k-forgejo "3.0.3-SNAPSHOT"
   :description "forgejo c4k-installation package"
   :url "https://domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
