633f450baf668db2f541f1a0ae3c6bd9185909da
Michael Jerger <michael.jerger@meissa-gmbh.de>
2024-01-26 18:15:31 +0100

    bump version to: 3.1.8-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.domaindrivenarchitecture/c4k-forgejo "3.1.7"
+(defproject org.domaindrivenarchitecture/c4k-forgejo "3.1.8-SNAPSHOT"
   :description "forgejo c4k-installation package"
   :url "https://domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
