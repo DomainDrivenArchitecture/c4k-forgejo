bff6788400da7223c901a3c4c531f7c0771b2797
Michael Jerger <michael.jerger@meissa-gmbh.de>
2024-02-12 15:44:41 +0100

    release: 3.1.10

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.domaindrivenarchitecture/c4k-forgejo "3.1.10-SNAPSHOT"
+(defproject org.domaindrivenarchitecture/c4k-forgejo "3.1.10"
   :description "forgejo c4k-installation package"
   :url "https://domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
