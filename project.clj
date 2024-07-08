3be34862020c295201c666820cff4e79815a0461
Clemens <clemens.geibel@meissa-gmbh.de.de>
2024-07-08 13:55:59 +0200

    bump version to: 3.3.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.domaindrivenarchitecture/c4k-forgejo "3.3.0"
+(defproject org.domaindrivenarchitecture/c4k-forgejo "3.3.1-SNAPSHOT"
   :description "forgejo c4k-installation package"
   :url "https://domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
