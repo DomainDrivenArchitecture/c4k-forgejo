786c06cc0ad10a7dbc672492b0a53aa54059955c
Clemens <clemens.geibel@meissa-gmbh.de.de>
2024-07-10 10:02:16 +0200

    release: 3.4.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.domaindrivenarchitecture/c4k-forgejo "3.4.4-SNAPSHOT"
+(defproject org.domaindrivenarchitecture/c4k-forgejo "3.4.4"
   :description "forgejo c4k-installation package"
   :url "https://domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
